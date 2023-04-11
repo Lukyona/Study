1df7511f0f5fe8b7b0f922d7c2e1d29e533ebd9d
SJ <sksmstnwjd1@naver.com>
2023-04-11 23:07:01 +0900

    .

--- c++ examples/c++ lectures/Project1/Project1/main_2.cpp
+++ c++ examples/c++ lectures/Project1/Project1/main_2.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <map>
 
+using std::map;
+using std::make_pair;
 
 
 int main()
 {
+    // 이진탐색 != 이진탐색트리, 이진탐색 자체는 트리가 아니어도 할 수 있음.
+    // 단, 정렬된 데이터에만 적용 가능
+    // 
+    // 이진탐색트리(BST)
+    // 데이터 입력 및 탐색 O(logN) 효율
+    // 트리의 모양이 밸런스가 유지되지 않으면 제대로 된 탐색 효율이 나오지 않는다.
+    // - 자가균형 기능 필요 (AVL, Red/Black tree)
 
 
 
 
 
 
-
-
-
+    return 0;
 }
